73fc519f910c9981f7811d44d10cd9f5db1c3834
shintarotakasaki <137417713+shintarotakasaki@users.noreply.github.com>
2025-01-31 15:39:30 +0900

    Update after_xl_scr.py

--- after_xl_scr.py
+++ after_xl_scr.py
@@ -11,31 +11,50 @@ from openpyxl import load_workbook
 import streamlit as st
 from io import BytesIO
 
-rects = []
-xlpoint = ["AC9-1","AC9","AC11","AC13","AC15","AC17","AC19","A11","S11","AM9"]
-xlvalue = []
+xlpoints = ["AC9-1","AC9","AC11","AC13","AC15","AC17","AC19","A11","S11","AM9"]
 
 def xl_data_upload(rects):
+    labels = []
+    values = []
     after_xl = st.file_uploader("アフター申請書エクセルをアップロードしてください")
     
     if after_xl is not None:
         file_mime = after_xl.type
         if file_mime == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-            file = BytesIO(after_xl.getvalue())
-            wb = load_workbook(filename=file)
-            sheet = wb.active
-            #st.write(f"Sheet title: {sheet.title}")
-            rects = ["AC9","AM9","AC11","AC13","AC15","AC19","AH3","A11","AB4"]
-            for rect in rects:
-                xlvalue.append(sheet[rect].value)
-            
+            try:
+                file = BytesIO(after_xl.getvalue())
+                wb = load_workbook(filename=file)
+                sheet = wb.active
+                #st.write(f"Sheet title: {sheet.title}")
+                rects = [
+                    ("G19","AC9-1"),
+                    ("G20","AC9"),
+                    ("L15","AM9"),
+                    ("N20","AC11"),
+                    ("G21","AC13"),
+                    ("N21","AC15"),
+                    ("G23","AC19-1"),
+                    ("Q23","AC19"),
+                    ("H28","A11")
+                        ]
+
+                for rect in rects:
+                    points,label = rect
+                    labels.append(label)
+                    for point in points:
+                        values.append(sheet[point].value)
+                        
+            except Exception as e:
+                st.error(f"エラーが発生しました: {e}")
+                return None, None
+                
         else:
             st.write("エクセルファイル(.xlsx)をアップロードしてください")
             st.stop()
 
 xl_data_get()
 
-def afterxl_dataget (xlvalue):
+def afterxl_dataget (values):
     import shutil
             github_url = "https://github.com/shintarotakasaki/excel3/raw/main/伝票(規格品)_ラベル_指示書.xlsm"
             # ファイルをダウンロードして一時ファイルとして保存
